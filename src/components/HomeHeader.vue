0309cd105eacc64026b436ac584ebf3f7acde2df
Muhammad Kholid Saifulloh <muhammadkholid@refactory.id>
2021-07-08 10:57:14 +0700

    init awal API

---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <img v-for="movie in movies" :key="movie.id">
+      <p>{{ movie.poster }}</p>
+      <p>{{ movie.title }}</p>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            movie: []
+        }
+    },
+
+    async mounted() {
+        const response = await axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=e99d5d2f')
+        this.movie = response.data
+    }
+}
+</script>
+
+<style>
+
+</style>
