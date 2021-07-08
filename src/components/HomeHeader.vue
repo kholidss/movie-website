9c464a2fea73c965156217e8bbb3383e5b26f91c
Muhammad Kholid Saifulloh <muhammadkholid@refactory.id>
2021-07-08 15:46:24 +0700

    sebelum card dg API

--- src/components/HomeHeader.vue
+++ src/components/HomeHeader.vue
@@ -3,7 +3,7 @@
         <img :src="movie.Poster" alt="Image" class="m-auto">
         <div class="mt-4">
             <p class="text-center text-5xl font-medium">{{ movie.Title }}</p>
-            <p class="text-center text-4xl font-normal mt-3">{{ movie.Year }}</p>
+            <p class="text-center text-4xl font-normalx` mt-3">{{ movie.Year }}</p>
         </div>
         <div>
             <p class="text-center mt-5 leading-8">{{ movie.Plot }}</p>
