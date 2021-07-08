c73bb7535934e271d60376c8051b166a5654b940
Muhammad Kholid Saifulloh <muhammadkholid@refactory.id>
2021-07-08 08:52:24 +0700

    desain navbar 1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <header class="flex justify-between align-center">
+    <router-link to="/">
+      <h1 class="text-black-500 text-4xl"><span>Nordic</span>Rose</h1>
+    </router-link>
+
+    <div class="flex mx-2 px-2">
+      <router-link to="/about" class="mx-2">
+        <h1 class="text-black-500"><span>About</span></h1>
+      </router-link>
+
+       <router-link to="/contact">
+        <h1 class="text-black-500"><span>Contact</span></h1>
+      </router-link>
+    </div>
+  </header>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ src/env.js
@@ -0,0 +1,3 @@
+export default {
+    apikey: "edd1ae84-8a2d-441b-8a4c-3d60832757aa"
+}
