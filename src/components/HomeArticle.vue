67113c3fc9cc86d3686b70a3df76a6df0dc95c06
Muhammad Kholid Saifulloh <muhammadkholid@refactory.id>
2021-07-08 16:05:29 +0700

    sesudah card dg API (tanpa looping)

--- src/components/HomeArticle.vue
+++ src/components/HomeArticle.vue
@@ -3,25 +3,53 @@
     <div class="mt-14 mb-5">
         <p class="text-center text-4xl font-semibold mb-9">All movies</p>
         <div class="flex justify-around items-center flex-wrap">
-            <div class="w-80 h-80 bg-green-700 mt-8">
 
+            <div class="w-80 h-80 mt-8">      
+                <img :src="movie.Poster" alt="Image" class="m-auto" width="190">                
+                <div class="mt-1">
+                    <p class="text-center font-normal text-2xl">{{ movie.Title }}</p>
+                </div>
             </div>
-            <div class="w-80 h-80 bg-green-700  mt-8">
 
+            <div class="w-80 h-80 mt-8">               
+                <img :src="movie.Poster" alt="Image" class="m-auto" width="190">
+                <div class="mt-1">
+                    <p class="text-center font-normal text-2xl">{{ movie.Title }}</p>
+                </div>
             </div>
-            <div class="w-80 h-80 bg-green-700  mt-8">
 
+            <div class="w-80 h-80 mt-8">               
+                <img :src="movie.Poster" alt="Image" class="m-auto" width="190">
+                <div class="mt-1">
+                    <p class="text-center font-normal text-2xl">{{ movie.Title }}</p>
+                </div>
             </div>
-            <div class="w-80 h-80 bg-green-700  mt-8">
 
+            <div class="w-80 h-80 mt-8">               
+                <img :src="movie.Poster" alt="Image" class="m-auto" width="190">
+                <div class="mt-1">
+                    <p class="text-center font-normal text-2xl">{{ movie.Title }}</p>
+                </div>
             </div>
+
         </div>
     </div>
     
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+    data() {
+        return {
+            movie: [],
+        }
+    },
+
+    async mounted() {
+        const response = await axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=e99d5d2f&t=peaky')
+        this.movie = response.data
+    }
 
 }
 </script>
