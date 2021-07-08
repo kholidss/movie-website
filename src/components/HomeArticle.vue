08b44af59e44923568b6eff5cef247cb9f806c2e
Muhammad Kholid Saifulloh <muhammadkholid@refactory.id>
2021-07-08 22:20:46 +0700

    sebelum loop range

--- src/components/HomeArticle.vue
+++ src/components/HomeArticle.vue
@@ -4,14 +4,25 @@
         <p class="text-center text-4xl font-semibold mb-9">All movies</p>
         <div class="flex justify-around items-center flex-wrap">
 
-            <div class="w-80 h-80 mt-8">      
-                <img :src="movie.Poster" alt="Image" class="m-auto" width="190">                
+            <!-- <div class="w-80 h-80 mt-8" v-for="(card,index) in cards" :key="index">      
+                {{card}}
+                <img :src="card.Poster" alt="Image" class="m-auto" width="190">                
                 <div class="mt-1">
                     <p class="text-center font-normal text-2xl">{{ movie.Title }}</p>
                 </div>
+                {}
+            </div> -->
+
+          
+
+            <div class="w-80 h-80 mt-8" v-for="(movie,index) in movies" :key="index">              
+                <img :src="movies.Poster" alt="Image" class="m-auto" width="190" :key="movie.id">
+                <div class="mt-1">
+                    <p class="text-center font-normal text-2xl">{{ movies.Title }}</p>
+                </div>
             </div>
 
-            <div class="w-80 h-80 mt-8">               
+            <!-- <div class="w-80 h-80 mt-8">               
                 <img :src="movie.Poster" alt="Image" class="m-auto" width="190">
                 <div class="mt-1">
                     <p class="text-center font-normal text-2xl">{{ movie.Title }}</p>
@@ -23,14 +34,7 @@
                 <div class="mt-1">
                     <p class="text-center font-normal text-2xl">{{ movie.Title }}</p>
                 </div>
-            </div>
-
-            <div class="w-80 h-80 mt-8">               
-                <img :src="movie.Poster" alt="Image" class="m-auto" width="190">
-                <div class="mt-1">
-                    <p class="text-center font-normal text-2xl">{{ movie.Title }}</p>
-                </div>
-            </div>
+            </div> -->
 
         </div>
     </div>
@@ -42,13 +46,13 @@ import axios from 'axios'
 export default {
     data() {
         return {
-            movie: [],
+            movies: [],
         }
     },
 
     async mounted() {
         const response = await axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=e99d5d2f&t=peaky')
-        this.movie = response.data
+        this.movies = response.data
     }
 
 }
